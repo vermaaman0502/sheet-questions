a476099e61a9f306a57e9342c62ace43d66afc9b
Aman <Amanverma210605@gmail.com>
2023-06-24 22:50:29 +0530

    Add Search key in a sorted matrix by Staircase approach

--- searchSortedMatrix.java
+++ searchSortedMatrix.java
@@ -1,6 +1,25 @@
 public class searchSortedMatrix {
+    public static boolean staircaseSearch(int matrix[][], int key) {
+        int row = 0,  col = matrix.length-1;
+        while(row<matrix.length && col>=0) {
+            if(key == matrix[row][col]) {
+                System.out.println(row + " "+ col);
+                return true;
+            }
+            else if(key< matrix[row][col]) {
+                col--;
+            }
+            else{
+                row++;
+            }
+        }
+        System.out.println("Not Found");
+        return false;
+    }
     public static void main(String[] args) {
-        int[][] matrix = {{1,3,5},{2,4,6}};
+        int[][] matrix = {{10,20,30,40},{15,25,35,45}, {27,29,37,48},{32,33,39,50}};
+        int key = 48;
+        staircaseSearch(matrix, key);
         
     }
     
