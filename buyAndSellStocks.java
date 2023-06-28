ef12001cc81e7df292a655a60d7c5ae2d26ac0ac
Aman <Amanverma210605@gmail.com>
2023-06-28 21:39:36 +0530

    Add buys and sell stock

---
+++ buyAndSellStocks.java
@@ -0,0 +1,22 @@
+public class buyAndSellStocks {
+    public static int bestTime(int prices[]) {
+        int maxProfit = 0;
+        int buyPrice = Integer.MAX_VALUE;
+        for(int i=0; i<prices.length; i++) {
+            if(buyPrice < prices[i]) {
+                int profit = prices[i] - buyPrice;
+                maxProfit = Math.max(maxProfit, profit);
+            }
+            else{
+                buyPrice = prices[i];
+            }
+        }
+        return maxProfit;
+
+    }
+    public static void main(String[] args) {
+        int prices[] = {7,1,5,3,6,4};
+        System.out.println(bestTime(prices));
+    }
+    
+}
