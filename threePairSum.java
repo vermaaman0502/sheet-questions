2c89fed91b163ae6f30dbecc6fdcbd308f09be2d
Aman <Amanverma210605@gmail.com>
2023-06-29 23:45:05 +0530

    Find three pair sum is equal to zero problem

---
+++ threePairSum.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class threePairSum {
+    public static boolean Sum(int arr[]) {
+        Arrays.sort(arr);
+        for(int i=0; i<arr.length-2; i++) {
+            int L = i+1,   R =arr.length-1;
+            while(L<R) {
+                if(arr[i] + arr[L] +arr[R] ==77) {
+                    return true;
+                } else if(arr[i] + arr[L] +arr[R] <0) {
+                    L++;
+                } else{
+                    R--;
+                }
+            }
+        }
+        return false;
+
+    }
+    public static void main(String[] args) {
+        int arr[] = {0,-1,2,-3,1};
+        System.out.println(Sum(arr));
+
+    }
+    
+}
